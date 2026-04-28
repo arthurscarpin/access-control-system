2e9c52990cb569fa94b1d3e8d583260619dba322
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2026-04-28 17:45:41 -0300

    feat(core): add custom exceptions to core module

---
+++ src/main/java/com/arthurscarpin/acs/core/exception/DuplicateLicensePlateException.java
@@ -0,0 +1,8 @@
+package com.arthurscarpin.acs.core.exception;
+
+public class DuplicateLicensePlateException extends RuntimeException {
+
+    public DuplicateLicensePlateException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/arthurscarpin/acs/core/exception/LicensePlateInvalidException.java
@@ -0,0 +1,8 @@
+package com.arthurscarpin.acs.core.exception;
+
+public class LicensePlateInvalidException extends RuntimeException {
+
+    public LicensePlateInvalidException(String message) {
+        super(message);
+    }
+}
