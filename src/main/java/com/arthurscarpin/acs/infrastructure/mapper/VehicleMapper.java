0969a9cd0d0f65bcf590a7d3b6d5ff26d6ed88de
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2026-04-29 03:14:18 -0300

    refactor(infrastructure/mapper): reorganize package structure

--- src/main/java/com/arthurscarpin/acs/infrastructure/mapper/VehicleMapper.java
+++ src/main/java/com/arthurscarpin/acs/infrastructure/mapper/VehicleMapper.java
@@ -1,11 +1,10 @@
 package com.arthurscarpin.acs.infrastructure.mapper;
 
-import com.arthurscarpin.acs.core.entity.Vehicle;
-import com.arthurscarpin.acs.core.usecases.registervehicle.RegisterVehicleRequest;
+import com.arthurscarpin.acs.core.vehicle.domain.Vehicle;
 import com.arthurscarpin.acs.infrastructure.persistence.entity.OwnerEntity;
 import com.arthurscarpin.acs.infrastructure.persistence.entity.VehicleEntity;
-import com.arthurscarpin.acs.infrastructure.dto.request.VehicleRequest;
-import com.arthurscarpin.acs.infrastructure.dto.response.VehicleResponse;
+import com.arthurscarpin.acs.infrastructure.presentation.request.VehicleRequest;
+import com.arthurscarpin.acs.infrastructure.presentation.response.VehicleResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -14,24 +13,16 @@ import java.util.UUID;
 @Mapper(componentModel = "spring")
 public interface VehicleMapper {
 
-    @Mapping(target = "owner", source = "ownerId")
-    VehicleEntity toEntity(Vehicle vehicle);
+    Vehicle fromRequestToDomain(VehicleRequest request);
 
-    @Mapping(target = "ownerId", source = "owner.id")
-    Vehicle toDomain(VehicleEntity entity);
+    VehicleResponse fromDomainToResponse(Vehicle vehicle);
 
-    RegisterVehicleRequest toRequest(VehicleRequest request);
+    VehicleEntity fromDomainToEntity(Vehicle vehicle);
 
-    VehicleResponse toResponse(Vehicle vehicle);
+    @Mapping(target = "ownerId", source = "owner")
+    Vehicle fromEntityToDomain(VehicleEntity entity);
 
-    default OwnerEntity map(UUID ownerId) {
-        if (ownerId == null) return null;
-        OwnerEntity owner = new OwnerEntity();
-        owner.setId(ownerId);
-        return owner;
-    }
-
-    default UUID map(OwnerEntity owner) {
+    default UUID mapOwnerToId(OwnerEntity owner) {
         return owner != null ? owner.getId() : null;
     }
 }
