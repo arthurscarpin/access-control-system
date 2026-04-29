349b6e5de357b21734f65cc62ae5eba5c1275540
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2026-04-29 13:32:39 -0300

    feat(vehicle): add UpdateVehicleStatusUseCase

--- src/main/java/com/arthurscarpin/acs/core/vehicle/usecase/UpdateVehicleStatusUseCase.java
+++ src/main/java/com/arthurscarpin/acs/core/vehicle/usecase/UpdateVehicleStatusUseCase.java
@@ -2,7 +2,9 @@ package com.arthurscarpin.acs.core.vehicle.usecase;
 
 import com.arthurscarpin.acs.core.vehicle.domain.Vehicle;
 
+import java.util.UUID;
+
 public interface UpdateVehicleStatusUseCase {
 
-    Vehicle execute(Vehicle vehicle);
+    Vehicle execute(UUID id);
 }
