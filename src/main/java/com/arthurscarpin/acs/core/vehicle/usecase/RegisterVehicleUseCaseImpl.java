c04e98a71335c0645d8dfc3aaa968a0b10b04d87
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2026-04-29 13:31:40 -0300

    refactor(vehicle): remove code smells from RegisterVehicleUseCaseImpl

--- src/main/java/com/arthurscarpin/acs/core/vehicle/usecase/RegisterVehicleUseCaseImpl.java
+++ src/main/java/com/arthurscarpin/acs/core/vehicle/usecase/RegisterVehicleUseCaseImpl.java
@@ -1,13 +1,12 @@
 package com.arthurscarpin.acs.core.vehicle.usecase;
 
 import com.arthurscarpin.acs.core.owner.domain.Owner;
-import com.arthurscarpin.acs.core.vehicle.domain.Vehicle;
-import com.arthurscarpin.acs.core.vehicle.domain.VehicleStatus;
-import com.arthurscarpin.acs.core.vehicle.exception.PlateDuplicateException;
 import com.arthurscarpin.acs.core.owner.exception.OwnerNotFoundException;
 import com.arthurscarpin.acs.core.owner.gateway.OwnerGateway;
-import com.arthurscarpin.acs.core.vehicle.gateway.VehicleGateway;
 import com.arthurscarpin.acs.core.vehicle.domain.Plate;
+import com.arthurscarpin.acs.core.vehicle.domain.Vehicle;
+import com.arthurscarpin.acs.core.vehicle.exception.PlateDuplicateException;
+import com.arthurscarpin.acs.core.vehicle.gateway.VehicleGateway;
 
 public class RegisterVehicleUseCaseImpl implements RegisterVehicleUseCase {
 
@@ -23,22 +22,12 @@ public class RegisterVehicleUseCaseImpl implements RegisterVehicleUseCase {
     @Override
     public Vehicle execute(Vehicle vehicle) {
         Plate plate = new Plate(vehicle.plate());
-
         if (vehicleGateway.findByPlate(plate.plate()).isPresent()) {
             throw new PlateDuplicateException("Plate already exists");
         }
-
         Owner owner = ownerGateway.findById(vehicle.ownerId())
                 .orElseThrow(() -> new OwnerNotFoundException("Owner not found"));
-
-        Vehicle vehicleToSave = new Vehicle(
-                vehicle.id(),
-                plate.plate(),
-                vehicle.model(),
-                VehicleStatus.ACTIVE,
-                owner.id()
-        );
-
-        return vehicleGateway.save(vehicleToSave);
+        Vehicle savedVehicle = Vehicle.create(plate, vehicle.model(), owner.id());
+        return vehicleGateway.save(savedVehicle);
     }
 }
