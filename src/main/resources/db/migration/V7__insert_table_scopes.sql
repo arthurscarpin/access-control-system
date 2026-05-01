1a67314f25fe9e1f8108dc46c2505184e01a453e
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2026-05-01 17:55:45 -0300

    chore(database): insert valid scopes

---
+++ src/main/resources/db/migration/V7__insert_table_scopes.sql
@@ -0,0 +1,6 @@
+-- Insert initial data into the scopes table
+INSERT INTO scopes (name) VALUES ('ADMIN');
+INSERT INTO scopes (name) VALUES ('USER');
+INSERT INTO scopes (name) VALUES ('READ');
+INSERT INTO scopes (name) VALUES ('WRITE');
+INSERT INTO scopes (name) VALUES ('DELETE');
