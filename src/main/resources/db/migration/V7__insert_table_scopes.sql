3b9df332e4667c4e50f3771424e2d573e6c8c18e
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2026-05-02 16:10:18 -0300

    chore(database): update insert scopes migration

--- src/main/resources/db/migration/V7__insert_table_scopes.sql
+++ src/main/resources/db/migration/V7__insert_table_scopes.sql
@@ -1,6 +1,8 @@
 -- Insert initial data into the scopes table
-INSERT INTO scopes (name) VALUES ('ADMIN');
-INSERT INTO scopes (name) VALUES ('USER');
-INSERT INTO scopes (name) VALUES ('READ');
-INSERT INTO scopes (name) VALUES ('WRITE');
-INSERT INTO scopes (name) VALUES ('DELETE');
+INSERT INTO scopes (name) VALUES ('admin:all');
+INSERT INTO scopes (name) VALUES ('vehicle:write');
+INSERT INTO scopes (name) VALUES ('vehicle:read');
+INSERT INTO scopes (name) VALUES ('owner:write');
+INSERT INTO scopes (name) VALUES ('owner:read');
+INSERT INTO scopes (name) VALUES ('access_event:write');
+INSERT INTO scopes (name) VALUES ('access_event:read');
